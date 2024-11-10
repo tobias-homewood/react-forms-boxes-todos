a34cfd0df195d8ed3ae9d7aabf5971d68153b20e
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-11-10 17:01:42 +0000

    box css and delete

--- src/Box.js
+++ src/Box.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import './Box.css'
 
-export default function Box({color, width, height, text}) {
+export default function Box({color, width, height, text, onClick}) {
   return (
-    <div className="card col-1 justify-content-center" style={{ width: `${width}px`, height: `${height}px`, backgroundColor: color }}>
-        <div className="card-body">
-            <p className="card-text text-center">{text}</p>
+    <div className="card col-1 justify-content-center" style={{ width: `${width}px`, height: `${height}px` }}>
+        <div className="card-header d-flex justify-content-end" style={{backgroundColor: 'beige'}}>
+          <button className="btn btn-danger btn-round" onClick={onClick}>X</button>
+        </div>
+        <div className="card-body" style={{backgroundColor: color}}>
+          <p className="card-text text-center">{text}</p>
         </div>
     </div>
   )
