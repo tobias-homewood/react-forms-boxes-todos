d24117ee325f297d72f431eaa18c135fbbdd238b
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-11-10 17:02:25 +0000

    PART 1 done: added missing delete function + using refactored components

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,23 +1,36 @@
-import React, { useState } from 'react';
-import Box from './Box';
-import NewBoxForm from './NewBoxForm';
+import React, { useState } from "react";
+import Box from "./Box";
+import NewBoxForm from "./NewBoxForm";
 
-export default function BoxList({fields}) {
+export default function BoxList() {
     const [boxes, setBoxes] = useState([]);
 
     const addBox = (newBox) => {
-        setBoxes(boxes => [...boxes, newBox]);
+        setBoxes((boxes) => [...boxes, newBox]);
+    };
+
+    const deleteBox = (index) => {
+        setBoxes((boxes) => boxes.filter((box, i) => i !== index));
     }
-  return (
-    <div>
-        {/* form to add new boxes */}
-        <NewBoxForm submitNewBox={addBox} fields={fields}/>
+    return (
+        <div className="container">
+            {/* form to add new boxes */}
+            <NewBoxForm submitNewBox={addBox} />
 
-        {/* the list of boxes rendered as a Box Component */}
-        {boxes.map((properties, i) => (
-            <Box key={i} properties={properties} />
-        ))}
-
-    </div>
-  )
+            {/* the list of boxes rendered as a Box Component */}
+            <div className="mt-4 row">
+                {boxes.map((box, i) => (
+                    <div key={i} className="col-md-3 mb-4">
+                        <Box
+                            width={box.width}
+                            height={box.height}
+                            color={box.color}
+                            text={box.text}
+                            onClick={() => deleteBox(i)}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
