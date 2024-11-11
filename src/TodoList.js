5c548a57645153aa104e7d2ec70f47918422f34f
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-11-11 21:16:28 +0000

    setting up editing functionality: state, toggles and so on

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import NewTodoForm from './NewTodoForm'
+import EditTodoForm from './EditTodoForm'
 import Todo from './Todo'
 
 export default function TodoList() {
     const [todos, setTodos] = useState([]);
+    const [showEditForm, setShowEditForm] = useState(false);
+    const [editIndex, setEditIndex] = useState(null);
 
     const addTodo = (newTodo) => {
         setTodos((todos) => [...todos, newTodo]);
@@ -12,11 +15,28 @@ export default function TodoList() {
     const deleteTodo = (index) => {
         setTodos((todos) => todos.filter((_todo, i) => i !== index));
     }
+
+    const editTodo = (index, newText) => {  
+        setTodos((todos) => todos.map((todo, i) => i === index ? {...todo, text: newText} : todo));
+    }
+
+    const toggleEditForm = (index) => {
+        setShowEditForm(!showEditForm);
+        setEditIndex(index);
+    }
     return (
         <div className="container">
             {/* form to add new todos */}
-            <h1 className="text-center">Create a new Todo</h1>
-            <NewTodoForm submitNewTodo={addTodo} />
+            {showEditForm ?
+                <h1 className="text-center">Editing</h1>
+                :
+                <h1 className="text-center">Create a new Todo</h1>
+            }
+            {showEditForm ?
+                <EditTodoForm editTodo={editTodo} toggleEditForm={toggleEditForm} index={editIndex} currentText={todos[editIndex].text} />
+                :
+                <NewTodoForm submitNewTodo={addTodo} />
+            }            
 
             {/* the list of todos rendered as a Todo Component */}
             <div className="mt-4 row">
@@ -24,6 +44,8 @@ export default function TodoList() {
                     <div key={i} className="col-md-3 mb-4">
                         <Todo
                             text={todo.text}
+                            beingEdited={i === editIndex}
+                            onEdit={() => toggleEditForm(i)}
                             onClick={() => deleteTodo(i)}
                         />
                     </div>
