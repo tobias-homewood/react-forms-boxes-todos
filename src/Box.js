ba51c1edb44ce1f65fe2b5349801f29a81ed7546
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-11-11 21:19:30 +0000

    every todo has the toggle and edit functions

--- src/Box.js
+++ src/Box.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Box.css'
 
-export default function Box({color, width, height, text, onClick}) {
+export default function Box({color, width, height, text, onClick, onEdit}) {
   return (
     <div className="card col-1 justify-content-center" style={{ width: `${width}px`, height: `${height}px` }}>
-        <div className="card-header d-flex justify-content-end" style={{backgroundColor: 'beige'}}>
+        <div className="card-header d-flex justify-content-between" style={{backgroundColor: 'beige'}}>
+          <button className="btn btn-secondary btn-round" onClick={onEdit}>Edit</button>
           <button className="btn btn-danger btn-round" onClick={onClick}>X</button>
         </div>
         <div className="card-body" style={{backgroundColor: color}}>
--- src/Todo.js
+++ src/Todo.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Box from './Box'
 
-export default function Todo({text, onClick}) {
+export default function Todo({text, onClick, onEdit, beingEdited}) {
   return (
-    <Box width={200} height={200} color={"yellow"} text={text} onClick={onClick} />
+    <Box width={200} height={200} color={beingEdited ? "cyan" : "yellow"} text={text} onClick={onClick} onEdit={onEdit} />
   )
 }
